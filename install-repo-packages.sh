7ac0fb6500cd79f5c5331f5c35afa36d34c5660a
Maarten Menheere <m.menheere@m2tec.nl>
2023-01-19 10:42:31 +0100

    initial commit

---
+++ install-repo-packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#
+# This file is part of the m2-kiosk-ansible distribution (https://github.com/M2tec/m2_kiosk_ansible).
+# Copyright (c) 2023 Maarten Menheere.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+set -x
+
+ansible-playbook -i prod playbook-m2tec-repo.yaml
